728ccca4a08f2593cf2d383c99fed6e7eb45e2ee
Olena Hryshchenko <grischenko.olena.v@gmail.com>
2026-06-21 18:39:05 +0300

    common functional for all users implemented

--- src/main/java/com/epam/finaltask/controller/CommonController.java
+++ src/main/java/com/epam/finaltask/controller/CommonController.java
@@ -1,7 +1,12 @@
 package com.epam.finaltask.controller;
 
+import com.epam.finaltask.dto.UserDTO;
+import com.epam.finaltask.model.Role;
+import com.epam.finaltask.service.UserService;
 import com.epam.finaltask.service.VoucherService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +14,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @AllArgsConstructor
 public class CommonController {
-    VoucherService voucherService;
+    private UserService userService;
+    private VoucherService voucherService;
 
-    @GetMapping("/dashboard")
-    public String viewDashboard(Model model){
+    @GetMapping("/")
+    public String handleIndex(Model model) {
         model.addAttribute("vouchers", voucherService.findAll());
-        return "user/dashboard";
+        return "index";
+    }
+
+    @GetMapping("/cabinet")
+    public String viewCabinet(@AuthenticationPrincipal UserDetails user){
+        String redirectUrl = "redirect:/";
+        if(user == null){
+            return redirectUrl;
+        }
+
+        UserDTO currentUser = userService.getUserByUsername(user.getUsername());
+        Role userRole = Role.valueOf(currentUser.getRole());
+        switch (userRole){
+            case USER -> redirectUrl = "redirect:/dashboard";
+            case ADMIN -> redirectUrl = "redirect:/users";
+            case MANAGER -> redirectUrl = "redirect:/tours";
+        }
+        return redirectUrl;
     }
 }
