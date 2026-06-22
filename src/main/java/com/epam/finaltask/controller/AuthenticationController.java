73add9852748776f939426991bb7b9edc7826eb8
Olena Hryshchenko <grischenko.olena.v@gmail.com>
2026-06-22 23:44:58 +0300

    user registration has been modified

--- src/main/java/com/epam/finaltask/controller/AuthenticationController.java
+++ src/main/java/com/epam/finaltask/controller/AuthenticationController.java
@@ -12,6 +12,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -78,7 +79,12 @@ public class AuthenticationController {
     }
 
     @PostMapping("/register")
-    public String handleRegister(@ModelAttribute("userDTO") @Valid UserDTO userDTO, Model model) {
+    public String handleRegister(@ModelAttribute("userDTO") @Valid UserDTO userDTO, BindingResult result, Model model) {
+        if(result.hasErrors()){
+            model.addAttribute("errorMessage", result.getFieldError().getDefaultMessage());
+            model.addAttribute("userDTO", userDTO);
+            return "auth/registrationForm";
+        }
         try {
             userService.register(userDTO);
             model.addAttribute(userDTO);
